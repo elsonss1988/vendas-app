667b6a09845de3962279de4f670c9438f0e05d4d
Elson <elsonss1988@gmail.com>
2025-01-28 11:45:04 -0300

    update: adicionado scritp de seed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+
+
+insert into tb_users(id,name,email,password,role) values(1, 'admin', 'admin@vendas.com','amen', 'admin');
+insert into tb_users(id,name,email,password,role) values(2, 'user', 'user@vendas.com','amen', 'user');
+insert into tb_users(id,name,email,password,role) values(3, 'manager', 'manager@vendas.com','amen', 'manager') ;
+
+-- insert into tb_users(id,name,email,password,role) values(3,'supply','admin2@admin.com','amem','admin')
