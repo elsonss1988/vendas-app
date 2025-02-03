9ef35a18038abcb31e30db52a96d6650586eca46
Elson <elsonss1988@gmail.com>
2025-02-03 16:54:36 -0300

    fix: corrigindo error de bean

--- src/main/java/com/unixverso/vendas/vendas_app/business/dto/UserResponseDTO.java
+++ src/main/java/com/unixverso/vendas/vendas_app/business/dto/UserResponseDTO.java
@@ -4,11 +4,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 @Getter
 @Setter
