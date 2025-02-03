cffc80dac8ca27d40d10cf1d740602990104cc6e
Elson <elsonss1988@gmail.com>
2025-02-03 20:50:38 -0300

    update: entity todo configurado

--- src/main/java/com/unixverso/vendas/vendas_app/infrastructure/model/entities/Todo.java
+++ src/main/java/com/unixverso/vendas/vendas_app/infrastructure/model/entities/Todo.java
@@ -3,20 +3,43 @@ package com.unixverso.vendas.vendas_app.infrastructure.model.entities;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@EqualsAndHashCode
 @Entity
 public class Todo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 100)
     private String title;
+
+    @Column(nullable = false)
     private LocalDateTime createdAt;
+
+    @Column(nullable = false)
     private LocalDate deadLine;
+
+    @Column(nullable = true)
     private LocalDate fineshedAt;
 
 }
